6b8cc9576d433ab9fab7ff76cb589f415e31e98e
Jacob Tabor <jtabor214@gmail.com>
2024-01-12 11:57:01 -0500

    Apply session token logic; validate users params; extend ApplicationController with login methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,71 @@
 class ApplicationController < ActionController::API
 include ActionController::RequestForgeryProtection 
 
+  rescue_from StandardError, with: :unhandled_error 
+  rescue_from ActionController::InvalidAuthenticityToken, with: :invalid_authenticity_token
+
   protect_from_forgery with: :exception
-  before_action :snake_case_params 
-  before_action :attach_authenticity_token
+  before_action :snake_case_params, :attach_authenticity_token
+
+  #For testing
+  def test
+    if params.has_key?(:login)
+      login!(CreateUser.first)
+    elseif params.has_key?(:logout)
+      logout! 
+    end 
+
+    if current_user 
+      render json: { user: current_user.slice('id', 'username', 'session_token')}
+    else 
+      render json: ['No current user']
+    end
+  end
+
+  def current_user 
+    @current_user ||= CreateUser.find_by(session_token: session[:session_token])
+  end
+
+  def require_logged_in 
+    if !logged_in? 
+      render json: {errors: ['Must be logged in']}, status: :unauthorized
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil 
+    @current_user = nil
+  end
+
 
   private 
   
+  def invalid_authenticity_token
+    render json: { message: 'Invalid authenticity token' },
+      status: :unprocessable_entity
+  end
+
+  def unhandled_error(error)
+    if request.accepts.first.html?
+      raise error 
+    else 
+      @message = "#{error.class} - #{error.message}"
+      @stack = Rails::BacktraceCleaner.new.clean(error.backtrace)
+      render 'api/errors/internal_server_error', status: :internal_server_error
+
+      logger.error "\n#{@message}:\n\t#{@stack.join("\n\t")}\n"
+    end 
+  end
+
   def snake_case_params
     params.deep_transform_keys!(&:underscore)
   end
--- app/models/create_user.rb
+++ app/models/create_user.rb
@@ -1,2 +1,39 @@
 class CreateUser < ApplicationRecord
+  has_secure_password 
+  before_validation :ensure_session_token
+
+  validates :email, uniqueness: true, length: { in: 3..100 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true, uniqueness: true, length: { in: 3..15 }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 7..40 }, allow_nil: true
+
+  def self.find_by_credentials(credential, password)
+    field = credential =~ URI::MailTo::EMAIL_REGEXP ? :email : :name
+    user = CreateUser.find_by(field => credential)
+    if user&.authenticate(password)
+      return user 
+    else 
+      nil 
+    end
+  end
+  
+  def reset_session_token! 
+    self.session_token = generate_unique_session_token
+    self.save!
+    session_token
+  end
+  
+  private 
+
+  def ensure_session_token 
+    self.session_token ||= generate_unique_session_token
+  end 
+  
+  def generate_unique_session_token
+    while true 
+      token = SecureRandom.urlsafe_base64 
+      return token unless CreateUser.exists?(session_token: token)
+    end 
+  end
+
 end
