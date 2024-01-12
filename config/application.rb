119deb5fd96f64af674e27cbe9373639d3dcd172
Jacob Tabor <jtabor214@gmail.com>
2024-01-12 10:55:58 -0500

    Change port location; initialize database; extend Gemfile; add middleware; transform response keys

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,11 @@ module Betsy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
-
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore,
+      key: '_betsy',
+      same_site: :lax, 
+      secure: Rails.env.production?
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
