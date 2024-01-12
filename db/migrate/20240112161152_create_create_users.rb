13ded78334bb008ff4f644e5b0fe12a201f32b07
Jacob Tabor <jtabor214@gmail.com>
2024-01-12 11:21:56 -0500

    Create and migrate users table

---
+++ db/migrate/20240112161152_create_create_users.rb
@@ -0,0 +1,12 @@
+class CreateCreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false, index: {unique: true}
+      t.string :name, null: false, index: {unique: true}
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+
+      t.timestamps
+    end
+  end
+end
