f6176102bf2478908e05c97752e78fcf55bfeaa5
Jacob Tabor <jtabor214@gmail.com>
2024-01-18 14:46:16 -0500

    Set product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+
+  validates :name, :description, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :seller_id, presence: true
+
+  belongs_to :seller
 end
