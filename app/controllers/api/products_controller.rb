63f40fd3af0aeb4fa19ebcaca46a6ed33cc699ef
Jacob Tabor <jtabor214@gmail.com>
2024-01-18 15:31:54 -0500

    Set actions in products_controller

---
+++ app/controllers/api/products_controller.rb
@@ -0,0 +1,57 @@
+class ProductsController < ApplicationController
+  before_action :require_login, only: [:create, :edit, :update, :destroy]
+
+
+  def index 
+    @products = Product.all
+    render :index 
+  end
+
+  def show 
+    @product = Product.find(params[:id])
+    render :show
+  end 
+
+  def create 
+    @product = Product.new(product_params)
+    if @product.save!
+      render :show 
+    else 
+      render json: { errors: @product.errors.full_messages }, status: 422
+    end
+  end
+
+  def edit 
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product = Poem.find_by(id: params[:id])
+    if @product.user_id == current_user.id 
+      if @poem.update(poem_params)
+        render :show 
+      else  
+        render json: { errors: @product.errors.full_messages }, status: 422
+      end 
+    end
+  end
+
+  def destroy 
+    @product = current_user.products.find_by(id: params[:id])
+    if @product&.destroy
+      render :show 
+    else  
+      render json: { errors: @product.errors.full_messages }, status: 422
+    end
+  end
+
+
+  private 
+
+  def product_params 
+    params.require(:product).permit(:name, :description, :price, :seller_id)
+  end
+
+
+end
