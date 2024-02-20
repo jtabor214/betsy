cc6429f0899d7ce419caa3d134325320b1ce2c46
Jacob Tabor <jtabor214@gmail.com>
2024-02-19 21:35:11 -0500

    CSS styling for ProductShow page

--- frontend/src/components/product/ProductShow.jsx
+++ frontend/src/components/product/ProductShow.jsx
@@ -3,9 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { selectProduct, fetchProduct } from "../../store/product";
 import ReviewsIndex from "../Review/ReviewIndex";
-
-
-
+import './ProductShow.css';
 
 const ProductShow = () => {
   const dispatch = useDispatch();
@@ -18,13 +16,21 @@ const ProductShow = () => {
 
 
   return (
-    <>
-      <img src={product?.photoUrl} alt="random image" />
-      <p>{product?.name}</p>
-      <p>{product?.description}</p>
-      <p>{product?.price}</p>
-      <p><ReviewsIndex /></p>
-    </>
+    <div className="product-page-body">
+      <div className="product-page-container">
+        <div className="product-visuals-container">
+          <img id="product-image" src={product?.photoUrl} alt="random image" />
+        </div>
+        <div className="product-info-container">
+          <p id="price">${product?.price}</p>
+          {/* <p id="">{product?.name}</p> */}
+          <p id="product-desc">{product?.description}</p>
+        </div>
+      </div>
+      <div className="product-reviews-container">
+        <p><ReviewsIndex /></p>
+      </div>
+    </div>
   );
 
 
