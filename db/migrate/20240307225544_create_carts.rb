3e31c205588d61d83c9fd42640717ad095ada415
Jake <jtabor214@gmail.com>
2024-03-12 17:03:34 -0400

    Removed incorrect table, migrates table with correct migration name

--- db/migrate/20240307225544_create_carts.rb
+++ db/migrate/20240307225544_create_carts.rb
@@ -1,8 +1,12 @@
 class CreateCarts < ActiveRecord::Migration[7.0]
   def change
+    drop_table :carts
     create_table :carts do |t|
-
+      t.integer :quantity, null: false
+      t.references :product, null: false, foreign_key: true 
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
+    add_index :carts, [:user_id, :product_id], unique: true
   end
 end
